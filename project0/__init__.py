cb53feb54f26453d3595dda0471204ad5d126935
Kovida <mothukurikovidasree@gmail.com>
2022-03-08 22:30:26 -0600

    final commit

--- project0/__init__.py
+++ project0/__init__.py
@@ -28,18 +28,21 @@ def extractincidents(data):
 
     # Get the first page
     page1 = pdfReader.getPage(0).extractText()
+    #sp_page1 = page1.strip()
     sp_page1 = page1.replace("NORMAN POLICE DEPARTMENT", "")
     sp_page2 = sp_page1.replace("Daily Incident Summary (Public)", "")
     sp_page3 = sp_page2.rstrip()
-    # print(sp_page2)
+    print(sp_page3)
+    sp_page3 = sp_page3 + "\n"
 
     # Now get all the other pages
     for pagenum in range(1, pagecount):
         sp_page3 = sp_page3 + pdfReader.getPage(pagenum).extractText()
+    print(sp_page3)
 
     # contains all cell values excluding date/time values
     s = re.split(r'\d*\/\d*\/\d{4}\s\d*:\d*', sp_page3)
-    # print(s)
+    #print(s)
 
     # contains only date/time values in a list
     f = re.findall(r'\d*\/\d*\/\d{4}\s\d*:\d*', sp_page3)
@@ -50,7 +53,7 @@ def extractincidents(data):
     for i in range(len(s)):
         l1.append(f[i] + s[i])
     # l1 has all the cell values in proper order of column names given in PDF
-    # print(l1)
+    #print(l1)
 
     lis = []
     for i in l1:
@@ -71,19 +74,21 @@ def extractincidents(data):
             i[2] = m
             del i[3]
 
+
     # to fill null values
     for i in range(len(temp)):
         j = len(temp[i])
         while len(temp[i]) < 5:
+            #print(temp[i])
             temp[i].append('null')
         num = temp[i][j - 1]
-        # print()
+        #print(num)
         temp[i].pop(j - 1)
         temp[i].append(num)
 
     for j in temp:
         lis.append(j)
-    # print(lis)
+    #print(lis)
 
     tlis = []
     for i in lis:
@@ -118,7 +123,7 @@ def populatedb(cur, tlis):
 def status(cur):
     stlist = []
     for row in cur.execute(
-            'SELECT nature, count(nature) FROM incidents where nature is not null GROUP BY nature ORDER BY count(nature) DESC,nature ASC'):
+            'SELECT nature, COUNT(nature) FROM incidents GROUP BY nature ORDER BY COUNT(nature) DESC,nature ASC'):
         stlist.append(row)
         print("%s|%d" % (row[0], row[1]))
 
