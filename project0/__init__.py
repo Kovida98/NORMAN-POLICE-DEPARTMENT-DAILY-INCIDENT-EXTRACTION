10e903c58df08598e6f9ebf6e02fa1a3a79c2cc5
Kovida <mothukurikovidasree@gmail.com>
2022-03-08 21:04:17 -0600

    final commit

--- project0/__init__.py
+++ project0/__init__.py
@@ -7,8 +7,7 @@ import sqlite3
 
 def fetchincidents(url):
     headers = {}
-    headers[
-        'User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
 
     data = urllib.request.urlopen(urllib.request.Request(url, headers=headers))
     return data
